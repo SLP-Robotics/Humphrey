a98279b5071eedb764e8bf95f4d065318d27a514
SLP-Robotics #8234 <58189157+SLP-Robotics@users.noreply.github.com>
2022-02-17 13:59:55 -0600

    Updated to WPILiB 2022
    Works now???

---
+++ FixedHumphrey/FixedHumphrey/src/main/java/frc/robot/subsystems/drivehumphrey.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+
+public class drivehumphrey {
+    WPI_VictorSPX rightMotor1 = new WPI_VictorSPX(0);
+    WPI_VictorSPX rightMotor2 = new WPI_VictorSPX(1);
+    WPI_VictorSPX leftMotor1 = new WPI_VictorSPX(2);
+    WPI_VictorSPX leftMotor2 = new WPI_VictorSPX(3);
+
+    public MotorControllerGroup leftSide = new MotorControllerGroup(leftMotor2, leftMotor1);
+    public MotorControllerGroup rightSide = new MotorControllerGroup(rightMotor2, rightMotor1);
+
+    public DifferentialDrive drive = new DifferentialDrive(leftSide, rightSide);
+    
+    public void drive(double speed, double rotation) {
+        
+        drive.arcadeDrive(speed * 0.75, rotation);
+
+    }
+}
