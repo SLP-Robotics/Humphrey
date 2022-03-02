ea9d73ae048ad97fc9c89b27dfbb15587ce381a9
SLP-Robotics #8234 <58189157+SLP-Robotics@users.noreply.github.com>
2022-03-01 18:05:00 -0600

    simple distance to speed

--- HumpreyTheRobot/src/main/java/frc/robot/subsystems/AimBot.java
+++ HumpreyTheRobot/src/main/java/frc/robot/subsystems/AimBot.java
@@ -3,9 +3,20 @@ package frc.robot.subsystems;
 public class AimBot{
 
     //look up table here
+    private static final float[] LOOKUP_TABLE = {};
+    private float getSpeedForDistance(float distance){
+        int index = (int)Math.floor(distance / SPACING);
+        if (index >= LOOKUP_TABLE.length){
+            return 5;
+            //change value 5 to faster if far away
+        }
+        return LOOKUP_TABLE[index];
+
+    }
     //motor (top, bottom)
 
     private static final float TURNING_ERROR_MARGIN = 0.0000001f;
+    private static final float SPACING = 0.25f;
 
     public void shooterMotorSpeed(float distance){
 
