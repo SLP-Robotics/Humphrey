23553a4c426e96a341b3250c29d9967d9d815411
SLP-Robotics #8234 <58189157+SLP-Robotics@users.noreply.github.com>
2022-03-08 16:48:36 -0600

    Some changes

--- HumpreyTheRobot/src/main/java/frc/robot/subsystems/HumphreyShooter.java
+++ HumpreyTheRobot/src/main/java/frc/robot/subsystems/HumphreyShooter.java
@@ -12,7 +12,7 @@ public class HumphreyShooter {
     static WPI_VictorSPX changingWheel = new WPI_VictorSPX(6);
 
     public static final double constantWheelSpeed = 1;//This we can preset until we find a value we like
-
+    //Based on the situation of the motors and their placement, we might need to invert this? So that the motors actually shoot the ball and dont just spin it in place
     public static DifferentialDrive drive = new DifferentialDrive(constantWheel, changingWheel);
 
     public static void shoot(double wheelSpeed){
