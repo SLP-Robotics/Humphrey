db95577a632dc7313dcd9bc12caa026ae6245189
SLP-Robotics #8234 <58189157+SLP-Robotics@users.noreply.github.com>
2022-03-09 16:35:54 -0600

    switched name to talonfx

--- HumpreyTheRobot/src/main/java/frc/robot/subsystems/HumphreyShooter.java
+++ HumpreyTheRobot/src/main/java/frc/robot/subsystems/HumphreyShooter.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -8,8 +9,8 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 
 public class HumphreyShooter {
-    static WPI_VictorSPX constantWheel = new WPI_VictorSPX(5);
-    static WPI_VictorSPX changingWheel = new WPI_VictorSPX(6);
+    static WPI_TalonFX constantWheel = new WPI_TalonFX(5);
+    static WPI_TalonFX changingWheel = new WPI_TalonFX(6);
 
     public static final double constantWheelSpeed = 1;//This we can preset until we find a value we like
     //Based on the situation of the motors and their placement, we might need to invert this? So that the motors actually shoot the ball and dont just spin it in place
