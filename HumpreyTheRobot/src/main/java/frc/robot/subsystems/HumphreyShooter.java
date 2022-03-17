4be9668d5d64238a07947b7316da6bf2542b59d3
SLP-Robotics #8234 <58189157+SLP-Robotics@users.noreply.github.com>
2022-03-17 18:22:55 -0500

    Updated shooting system with a third motor, as per the build team's description of the shooting system with three motors

--- HumpreyTheRobot/src/main/java/frc/robot/subsystems/HumphreyShooter.java
+++ HumpreyTheRobot/src/main/java/frc/robot/subsystems/HumphreyShooter.java
@@ -11,13 +11,17 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 public class HumphreyShooter {
     static WPI_TalonFX constantWheel = new WPI_TalonFX(5);
     static WPI_TalonFX changingWheel = new WPI_TalonFX(6);
+    static WPI_TalonFX intakeWheel = new WPI_TalonFX(7);
 
     public static final double constantWheelSpeed = 0.5;//This we can preset until we find a value we like
     //Based on the situation of the motors and their placement, we might need to invert this? So that the motors actually shoot the ball and dont just spin it in place
     public static DifferentialDrive drive = new DifferentialDrive(constantWheel, changingWheel);
 
+    public static DifferentialDrive instakeSys = new DifferentialDrive(intakeWheel, null);
+
     public static void shoot(double wheelSpeed){
         drive.tankDrive(constantWheelSpeed, wheelSpeed);
+        instakeSys.tankDrive(1, 0);
         System.out.println("shooting: " + constantWheelSpeed + ", " + wheelSpeed);
     }
 }
--- HumpreyTheRobot/src/main/java/frc/robot/subsystems/Intake.java
+++ HumpreyTheRobot/src/main/java/frc/robot/subsystems/Intake.java
@@ -10,8 +10,8 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 public class Intake {
 
     //change motor names and change values
-    static WPI_VictorSPX intakeMotor1 = new WPI_VictorSPX(7);
-    static WPI_VictorSPX intakeMotor2 = new WPI_VictorSPX(8);
+    static WPI_VictorSPX intakeMotor1 = new WPI_VictorSPX(8);
+    static WPI_VictorSPX intakeMotor2 = new WPI_VictorSPX(9);
    
     public static final double intakeWheelSpeed = 0.1;
     //I don't know how fast this needs to be, I have it set really slow so nothing breaks    
